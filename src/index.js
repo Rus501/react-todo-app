4b0412aa176b1cafe3ccf4dc0654bada7378f46d
Rus_I <r.angaev9@gmail.com>
2021-09-07 14:29:20 +0300

    Revert router to initial state

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 //component file
 import TodoContainer from './functionBased/components/TodoContainer'
 
@@ -9,7 +9,7 @@ import './functionBased/App.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
